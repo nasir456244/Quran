ba70a0d67f8620482afdf07565f544e85ad220ca
nasir456244 <arturlegendflame@gmail.com>
2023-01-23 20:00:49 +1000

    code refactoring

--- pages/api/addChapters.js
+++ pages/api/addChapters.js
@@ -3,9 +3,7 @@ export default async function handler(req, res) {
     try {
         
         const chapters = JSON.parse(req.body)
-        await chapters?.map((chapter) => db.collection('chapters').doc(`chapter ${chapter?.id}`).set(chapter))
-        // await db.collection("chapters").doc(`chapter${chapters?.id}`).set(chapters)
-        
+        await chapters?.map((chapter) => db.collection('chapters').doc(`chapter ${chapter?.id}`).set(chapter))        
         res.status(201).send('lol')
     }
     catch(err) {
